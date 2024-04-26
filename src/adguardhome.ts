07edab59f8af8bf03181291017f0b2b121cddbbf
Steve Molloy <smolloy@hotmail.com>
2024-04-26 08:48:40 -0700

    Initial commit of AGH+: Switch, Lock, TV - Timers, Un/Bridged

---
+++ src/adguardhome.ts
@@ -0,0 +1,250 @@
+import got, { CancelableRequest, Got } from 'got';
+import { Logger } from 'homebridge';
+
+export class AdGuardStatus {
+  public isAvailable: boolean | undefined;
+  public error;
+  public enabled: boolean | undefined;
+  public blocked_services: string[] = [];
+  public blocked_clients: string[] = [];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public clients: any[] = [];
+}
+
+export default class AdGuardHomeServer {
+
+  private readonly aghApi: Got;
+  private readonly latest: AdGuardStatus = new AdGuardStatus();
+
+  constructor(
+    public readonly host: string,
+    public readonly port: string,
+    public readonly https: boolean,
+    public readonly username: string,
+    public readonly password: string,
+    public readonly timeout: number,
+    public readonly log: Logger,
+  ) {
+    const prefixUrl = `http${https ? "s" : ""}://${host}:${port}/control`;
+    const Authorization = `Basic ${Buffer.from(
+      `${username}:${password}`
+    ).toString("base64")}`;
+
+    this.aghApi = got.extend({
+      // eslint-disable-next-line prettier/prettier
+      prefixUrl: prefixUrl,
+      responseType: "json",
+      timeout: timeout,
+      retry: {
+        limit: 0
+      },
+      headers: {
+        Authorization,
+      },
+      https: {
+        rejectUnauthorized: false,
+      },
+    });
+
+    this.log.debug(`AdGuard Http client created: [${prefixUrl}]`);
+  }
+
+  public async getCurrentStatus(getStatus: boolean, getDefaultServices: boolean, getDisallowedClients: boolean, getClientServices: boolean)
+    : Promise<AdGuardStatus>
+  {
+    const currentStatus = new AdGuardStatus;
+    currentStatus.isAvailable = true;
+
+    this.log.debug(`AGH: Querying AGHome server for current status: [${getStatus}, ${getDefaultServices}, ${getDisallowedClients}, ${getClientServices}]...`);
+    let abortPromise: boolean = false;
+    await Promise.all([
+      getStatus ? this.aghApi("status")
+        .json()
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        .then((body: any) => { if (!abortPromise) { this.latest.enabled = currentStatus.enabled = body.protection_enabled; } })
+        : Promise.resolve(),
+
+      getDefaultServices ? this.aghApi("blocked_services/get")
+        .json()
+        // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        .then((body: any) => { if (!abortPromise) { this.latest.blocked_services = currentStatus.blocked_services = body.ids; } })
+        : Promise.resolve(),
+
+      getDisallowedClients ? this.aghApi("access/list")
+        .json()
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        .then((body: any) => { if (!abortPromise) { this.latest.blocked_clients = currentStatus.blocked_clients = body.disallowed_clients; } })
+        : Promise.resolve(),
+
+      getClientServices ? this.aghApi("clients")
+        .json()
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        .then((body: any) => { if (!abortPromise) { this.latest.clients = currentStatus.clients = body.clients; } })
+        : Promise.resolve(),
+    ])
+    .catch((error) => {
+      abortPromise = true;
+      this.log.debug("AGH: AGHome server is unreachable!");
+      currentStatus.isAvailable = false;
+      currentStatus.error = (error.response) ? error.response.body : error;
+    })
+    .then(() => {
+      this.log.debug("AGH: All requested statuses have been received.")
+      return currentStatus;
+    });
+
+    return currentStatus;
+  }
+
+  public async postGlobal(enabled: boolean): Promise<boolean>
+  {
+    this.log.info(`AGH: Setting Global status to: ${enabled}`);
+
+    return this.doPostWrapper(
+      this.aghApi.post("dns_config", {
+        json: { protection_enabled: enabled },
+        headers: { "X-homebridge-aghp-info": `global - ${enabled}` },
+      })
+    );
+  }
+
+  public async postGlobalServices(enabled: boolean, services: string[]): Promise<boolean>
+  {
+    this.log.info(`AGH: Setting Global Services status to: ${enabled} - [${services.join(',')}]`);
+
+    const newServiceList = enabled ? this.merge(this.latest.blocked_services, services)
+      : this.remove(this.latest.blocked_services, services);
+    return this.doPostWrapper(
+      this.aghApi.put("blocked_services/update", {
+        json: { ids: newServiceList },
+        headers: { "X-homebridge-aghp-info": `global-services - ${enabled} [${services.join(',')}]` },
+      })
+    );
+  }
+
+  public async postClients(enabled: boolean, clients: string[]): Promise<boolean>
+  {
+    this.log.info(`AGH: Setting Client status to: ${enabled} - [${clients.join(',')}]`);
+
+    const clientList = this.expandTags(clients);
+    const newClientList = enabled ? this.merge(this.latest.blocked_clients, clientList)
+      : this.remove(this.latest.blocked_clients, clientList);
+    return this.doPostWrapper(
+      this.aghApi.post("access/set", {
+        json: { disallowed_clients: newClientList },
+        headers: { "X-homebridge-aghp-info": `client - ${enabled} [${clients.join(',')}]` },
+      })
+    );
+  }
+
+  public async postClientServices(enabled: boolean, clients: string[], services: string[]): Promise<boolean>
+  {
+    this.log.info(`AGH: ${enabled ? 'Blocking' : 'Unblocking'} Client Services for: ${clients.join(',')}`);
+    this.log.info(`     Services: [${services.join(',')}]`);
+
+    const clientList = this.expandTags(clients);
+    const postList = clientList.map((cname) => this.latest.clients.find((c) => c.name === cname))
+      .filter((cc) => cc != null)
+      .map(async (clientConfig) => {
+        const newServiceList = enabled ?
+          this.merge(clientConfig.blocked_services, services)
+          : this.remove(clientConfig.blocked_services, services);
+        const newData = JSON.parse(JSON.stringify(clientConfig));
+        newData.blocked_services = newServiceList;
+        const newConfig = { name: clientConfig.name, data: newData };
+        this.log.debug(`AGH: creating a promise to post to 'clients/update' for client ${clientConfig.name}`);
+        return this.aghApi.post("clients/update", {
+          json: newConfig,
+          headers: { "X-homebridge-aghp-info": `client-services - ${enabled} [${clientConfig.name}]` },
+        });
+      }
+    );
+
+    return this.doPostWrapper(Promise.allSettled(postList));
+  }
+
+  private async doPostWrapper(post: Promise<any>): Promise<boolean>
+  {
+    // Should type-check the array items with '.every'... but  we know we're only going to get an array
+    // here from Promise.allSettled(). Single promises return http response objects, not arrays.
+    var success: boolean = await post.then((result) => {
+      // Check that all promises succeeded
+      if (Array.isArray(result))
+      {
+        //const isFulfilled = <T,>(p:PromiseSettledResult<T>): p is PromiseFulfilledResult<T> => p.status === 'fulfilled';
+        const isRejected = <T,>(p:PromiseSettledResult<T>): p is PromiseRejectedResult => p.status === 'rejected';
+
+        const failed = result.filter(isRejected);
+        if (failed.length > 0)
+        {
+          let failedResponses = failed.map(p => p.reason); // Get actual results out of PromiseSettledResult<T>
+          failedResponses.forEach((r) => {
+            // I don't know what the shape of 'reason' is. Let's just see what it says.
+            this.log.warn('AGH: Promise.allSettled request failed.');
+            this.log.error(r);
+          });
+          return false;
+        }
+
+        this.log.debug('AGH: Returning true after finding no errors in Promise.allSettled.');
+        return true;
+      }
+
+      // Otherwise, we can simply check the status code
+      if (result.statusCode === 200)
+        return true;
+
+      this.log.info(`AGH: Request returned non-200 status code: ${result.statusCode}`);
+      if (result.request) this.log.debug(`AGH: Headers == ${result.request.headers}`);
+      this.log.error(result.response ? result.response.body : result);
+      return false;
+    })
+    .catch((error) => {
+      this.log.info(`AGH: Request failed with an error: ${error.code}`);
+      if (error.request) this.log.debug(`AGH: Headers == ${error.request.headers}`);
+      this.log.error(error.response ? error.response.body : error);
+      return false;
+    });
+
+    return success;
+  }
+
+  public expandTags(clientList: string[]): string[]
+  {
+    const newClientList: string[] = [];
+
+    if (!this.latest.clients)
+      return newClientList;
+
+    clientList.forEach((c) => {
+      if (c.startsWith('@')) {
+        this.latest.clients.filter((cstat) => cstat.tags.includes(c.substring(1))).forEach((exp) => {
+          newClientList.push(exp.name);
+        });
+      } else {
+        newClientList.push(c);
+      }
+    });
+
+    return newClientList;
+  }
+
+  private merge(list1: string[], list2: string[], sort: boolean = false): string[]
+  {
+    const newList: string[] = [];
+    list1.forEach((s) => newList.push(s));
+    list2.forEach((s) => { if (!newList.includes(s)) newList.push(s); });
+
+    if (sort)
+      newList.sort((a, b) => (a > b ? 1 : -1));
+
+    return newList;
+  }
+
+  private remove(oldList: string[], subList: string[]): string[]
+  {
+    const newList: string[] = [];
+    oldList.forEach((s) => { if (!subList.includes(s)) newList.push(s); });
+    return newList;
+  }
+}
--- src/platform.ts
+++ src/platform.ts
@@ -1,35 +1,77 @@
-import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
+import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic, Categories } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import AGHGroup from './platformAccessory';
+import AGH, { AdGuardStatus as AGHStatus, AdGuardStatus } from './adguardhome';
+
+// TODO smolloy:
+// locks/races?
+// final logging check
+
 
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class AdGuardHomePlus implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
   // this is used to track restored cached accessories
   public readonly accessories: PlatformAccessory[] = [];
+  private readonly switchGroups: AGHGroup[] = [];
+
+  // AdGuard Home server connection
+  private readonly host: string;
+  private readonly port: string;
+  private readonly https: boolean;
+  private readonly username: string;
+  private readonly password: string;
+  private readonly interval: number;
+  private readonly statusTimeout: number;
+
+  // Misc
+  private readonly name: string;
+  private readonly noCache: boolean;
+  private readonly clearUnusedCache: boolean;
+  private readonly agh: AGH;
+  private lastStatus: AdGuardStatus = new AdGuardStatus();
 
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
   ) {
-    this.log.debug('Finished initializing platform:', this.config.name);
+    this.name = this.config.name!;
+    this.host = this.config["host"] || "localhost";
+    this.port = this.config["port"] || 80;
+    this.https = !!this.config["https"];
+    this.username = this.config["username"];
+    this.password = this.config["password"];
+    this.statusTimeout = this.config["statusTimeout"] || 7500;
+    this.interval = this.config["interval"] || 10000;
+    this.noCache = !(this.config["useCache"] || false); // !(false, unless explicity set to true)
+    this.clearUnusedCache = (this.config["clearUnusedCache"] === false); // true, unless explicity set to false
+
+    this.agh = new AGH(this.host, this.port, this.https, this.username, this.password, this.statusTimeout, this.log);
+
+    this.log.debug('Finished initializing platform:', this.name);
 
     // When this event is fired it means Homebridge has restored all cached accessories from disk.
     // Dynamic Platform plugins should only register new accessories after this event was fired,
     // in order to ensure they weren't added to homebridge already. This event can also be used
     // to start discovery of new accessories.
-    this.api.on('didFinishLaunching', () => {
-      log.debug('Executed didFinishLaunching callback');
+    this.api.on('didFinishLaunching', async () => {
+      log.debug('Executing didFinishLaunching callback');
+      // Get initial AGH status
+      var initialStatus: AdGuardStatus = await this.agh.getCurrentStatus(true, true, true, true);
       // run the method to discover / register your devices as accessories
-      this.discoverDevices();
+      this.discoverDevices(initialStatus!);
+      // keep switch state in sync with the current AdGuardHome status
+      this.pollAdGuardStatusLoop();
+      // restore/finish any timers that were left unfinished when last shutdown
+      this.restoreUnfinishedTimers();
     });
   }
 
@@ -45,72 +87,151 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
   }
 
   /**
-   * This is an example method showing how to register discovered accessories.
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-  discoverDevices() {
+  discoverDevices(initialStatus: AdGuardStatus) {
 
-    // EXAMPLE ONLY
-    // A real plugin you would discover accessories from the local network, cloud services
-    // or a user-defined array in the platform config.
-    const exampleDevices = [
-      {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
-    ];
+    if (!this.config["switches"])
+      return;
 
-    // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    const configuredUUIDs: string[] = [];
 
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+    // Loop over configured switches and register each one if it has not already been registered
+    for (const switchConfig of this.config["switches"])
+    {
+      // If the switch is unbridged, each timer needs it's own accessory
+      const isBridged = !(switchConfig['bridged'] === false);
+      const originalTimeouts: string = switchConfig['autoResetTimes'];
+      let actualTimeouts: string[] = [originalTimeouts];
+      if (!isBridged)
+        actualTimeouts = (switchConfig['autoResetTimes'] === undefined) ? ['0'] : switchConfig['autoResetTimes'].split(',');
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+      actualTimeouts.forEach((timeouts) => {
+        // generate a unique id for the accessory
+        let timerVal = parseInt(timeouts);
+        const accessoryName = (isBridged || (!isNaN(timerVal) && timerVal === 0)) ? switchConfig.name : `${switchConfig.name}: ${timerVal} minute${timerVal > 1 ? 's' : ''}`;
+        const uuid = this.api.hap.uuid.generate(accessoryName);
+        configuredUUIDs.push(uuid);
 
-      if (existingAccessory) {
-        // the accessory already exists
-        this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+        // see if a switch group with the same uuid has already been registered and restored from
+        // the cached devices we stored in the `configureAccessory` method above
+        const existingGroup = this.accessories.find(accessory => accessory.UUID === uuid);
 
-        // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-        // existingAccessory.context.device = device;
-        // this.api.updatePlatformAccessories([existingAccessory]);
+        if (existingGroup && !this.noCache)
+        {
+          // the switch group already exists
+          this.log.info('Restoring existing switch group from cache:', existingGroup.displayName);
 
-        // create the accessory handler for the restored accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+          // update the accessory.context and run `api.updatePlatformAccessories`. eg.:
+          existingGroup.context.config = switchConfig;
+          this.api.updatePlatformAccessories([existingGroup]);
+          
+          // create the accessory handler for the restored accessory
+          // this is imported from `platformAccessory.ts`
+          this.switchGroups.push(new AGHGroup(this, existingGroup, initialStatus, this.agh));
+        }
+        else
+        {
+          // For debugging: remove the cached switch group and start from scratch
+          // that way there won't be any artifacts attached to the group from previous
+          // debug attempts that found their way into the accessory cache.
+          if (this.noCache && existingGroup)
+          {
+            this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingGroup]);
+            this.log.info('Removing existing switch from cache:', existingGroup.displayName);
+          }
 
-        // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-        // remove platform accessories when no longer present
-        // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-        // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+          // the switch group does not yet exist, so we need to create it
+          this.log.info('Adding new switch group:', switchConfig.name);
 
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+          // create a new platform accessory
+          const newGroup = new this.api.platformAccessory(accessoryName, uuid, switchConfig['category'] || Categories.PROGRAMMABLE_SWITCH);
+          switchConfig['autoResetTimes'] = timeouts;
+          newGroup.context.config = switchConfig;
+          this.switchGroups.push(new AGHGroup(this, newGroup, initialStatus, this.agh));
 
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
-
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
-
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-      }
+          // link the accessory to your platform - or not
+          if (isBridged)
+            this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [newGroup]);
+          else
+            this.api.publishExternalAccessories(PLUGIN_NAME, [newGroup]);
+        }
+      });
     }
+
+    // All our configured switches are created. Clear out the cache of any we didn't re-hydrate.
+    if (this.clearUnusedCache)
+    {
+      this.accessories.forEach((accessory) => {
+        if (!configuredUUIDs.find(uuid => uuid === accessory.UUID))
+        {
+          this.log.info(`Removing unused accessory '${accessory.displayName}' from cache.`);
+          this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        }
+      });
+    }
+  }
+
+  private pollAdGuardStatusLoop() {
+    this.log.info(`Starting AdGuard Home+ monitoring loop with interval ${this.interval}...`);
+
+    setInterval(async () => {
+      let [getStatus, getDefaultServices, getDisallowedClients, getClientServices]: boolean[] = [false, false, false, false];
+      this.switchGroups.forEach((ags) => {
+        if (ags.isGlobal) {
+          getStatus ||= !ags.isSelectServices;
+          getDefaultServices ||= ags.isSelectServices;
+        } else {
+          getClientServices = true; // Needed in both client cases for @tag expansion.
+          getDisallowedClients ||= !ags.isSelectServices;
+        }
+      });
+
+      this.log.debug(`Checking AGH status [${getStatus}, ${getDefaultServices}, ${getDisallowedClients}, ${getClientServices}]...`);
+      var currentStatus: AdGuardStatus = await this.agh.getCurrentStatus(getStatus, getDefaultServices, getDisallowedClients, getClientServices);
+
+      if (currentStatus.isAvailable === true && this.lastStatus.isAvailable !== true)
+      {
+        this.log.info("AdGuardHome Server is now responsive.");
+      }
+      else if (currentStatus.isAvailable !== true && this.lastStatus.isAvailable === true)
+      {
+        this.log.info("AdGuardHome Server is not responding:");
+        if (currentStatus.error)
+          this.log.error(currentStatus.error);
+      }
+
+      this.updateSwitchGroups(currentStatus)
+      .catch((error) => {
+        this.log.error("Failed to update switch states!");
+        this.log.error(error.response ? error.response.body : error);
+        currentStatus.isAvailable = false;
+        this.updateSwitchGroups(currentStatus)
+        .catch((e2) => {
+          this.log.error("DOUBLE FAIL!! Switches still not updated! ");
+          this.log.error(e2.response ? e2.response.body : e2);
+        });
+      });
+      this.lastStatus = currentStatus;
+    }, this.interval);
+  }
+
+  private async restoreUnfinishedTimers()
+  {
+    this.switchGroups.forEach(async (group) => {
+      this.log.debug(`Restoring timers for '${group.name}'...`);
+      await group.restoreUnfinishedTimers();
+      this.log.debug(`  Done restoring timers for '${group.name}'`);
+    });
+  }
+
+  private async updateSwitchGroups(currentStatus: AGHStatus)
+  {
+    this.switchGroups.forEach((group) => {
+      this.log.debug(`Updating platform status for '${group.name}' (IsAvailable: ${currentStatus.isAvailable})...`)
+      group.update(currentStatus);
+      this.log.debug(`  Done updating platform status for '${group.name}' (IsAvailable: ${currentStatus.isAvailable}).`);
+    });
   }
 }
